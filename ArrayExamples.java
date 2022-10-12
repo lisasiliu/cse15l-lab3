ef2e06a95688fcb189a1f83bdb7416ca0cf80075
Lisa Liu <44093048+lisasiliu@users.noreply.github.com>
2022-10-12 16:47:42 -0700

    Update ArrayExamples.java
    checkpoint 1
    all of arrayexamples's methods work now

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,9 +4,19 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    for(int i = 0; i < arr.length / 2; i += 1) {
+      int tmp = arr[i];
       arr[i] = arr[arr.length - i - 1];
-    }
+      arr[arr.length - i - 1] = tmp;
+    } 
+
+    /*
+     * error: overwrites arr[i] on first half without saving it for assignment later
+     * original:
+      for(int i = 0; i < arr.length; i += 1) {
+        arr[i] = arr[arr.length - i - 1];
+      }
+    */
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
@@ -14,9 +24,20 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
+
+    /*
+     * error: Values from the empty new array are being assigned to the old array, 
+              and it should be the other way around.
+     * original:
+        int[] newArray = new int[arr.length];
+        for(int i = 0; i < arr.length; i += 1) {
+          arr[i] = newArray[arr.length - i - 1];
+        }
+        return arr;
+    */
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
@@ -30,9 +51,27 @@ public class ArrayExamples {
     }
     double sum = 0;
     for(double num: arr) {
-      if(num != lowest) { sum += num; }
+      sum += num;
     }
+    sum -= lowest;
     return sum / (arr.length - 1);
+
+    /*
+     * error: Same number might get skipped if equal to the lowest 
+              (only one of the lowest number should be dropped, 
+              but function will drop duplicates too)
+     * original:
+      if(arr.length < 2) { return 0.0; }
+        double lowest = arr[0];
+      for(double num: arr) {
+        if(num < lowest) { lowest = num; }
+      }
+      double sum = 0;
+      for(double num: arr) {
+        if(num != lowest) { sum += num; }
+      }
+      return sum / (arr.length - 1);
+    */
   }
 
 
